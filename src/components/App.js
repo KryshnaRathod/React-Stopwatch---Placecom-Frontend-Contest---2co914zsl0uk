1ce0a6f636a7232081fb7b77bdfd9b7158873ea1
Krishna Rathod <88035445+KryshnaRathod@users.noreply.github.com>
2023-07-15 11:26:00 +0530

    Updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,62 +2,64 @@ import React, { useRef, useState } from 'react';
 import '../styles/App.css';
 
 
-function App() {
+const App = () => {
   const [time, setTime] = useState(0);
   const [laps, setLaps] = useState([]);
   const intervalRef = useRef(null);
 
-  const startStopwatch = () => {
-    if (intervalRef.current === null) {
-      intervalRef.current = setInterval(() => {
-        setTime((prevTime) => prevTime + 0.01);
-      }, 10);
-    }
+  const startTimer = () => {
+    intervalRef.current = setInterval(() => {
+      setTime((prevTime) => prevTime + 0.01);
+    }, 10);
   };
 
-  const stopStopwatch = () => {
+  const stopTimer = () => {
     clearInterval(intervalRef.current);
-    intervalRef.current = null;
   };
 
-  const lapStopwatch = () => {
+  const lapTimer = () => {
     setLaps((prevLaps) => [...prevLaps, time.toFixed(3)]);
   };
 
-  const resetStopwatch = () => {
+  const resetTimer = () => {
+    stopTimer();
     setTime(0);
     setLaps([]);
-    clearInterval(intervalRef.current);
-    intervalRef.current = null;
   };
 
   return (
-    <div>
+    <div className="App">
       <h1>Stopwatch</h1>
-      <h2>{time.toFixed(3)}</h2>
-      <div>
-        <button onClick={startStopwatch}>START</button>
-        <button onClick={stopStopwatch}>STOP</button>
-        <button onClick={lapStopwatch}>LAP</button>
-        <button onClick={resetStopwatch}>RESET</button>
+      <div className="timer">{time.toFixed(3)}</div>
+      <div className="controls">
+        <button onClick={startTimer}>START</button>
+        <button onClick={stopTimer}>STOP</button>
+        <button onClick={lapTimer}>LAP</button>
+        <button onClick={resetTimer}>RESET</button>
       </div>
-      <div
-        className="lap-section"
-        style={{ display: laps.length > 0 ? "block" : "none" }}
-      >
-        <h3>Laps:</h3>
-        <ul className="laps">
-          {laps.map((lap, index) => (
-            <li key={index}>{lap}</li>
-          ))}
-        </ul>
+      <div className="lap-section">
+        {laps.length > 0 && (
+          <div>
+            <h2>Lap Times</h2>
+            <ul className="laps">
+              {laps.map((lap, index) => (
+                <li key={index}>{lap}</li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default App;
 
+
+
+
+ 
+     
 
 
   
